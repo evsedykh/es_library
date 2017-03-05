81ddbc43d78e872a64b53d142bb60ebeb6952a47
eugene <sedykh.eugene@gmail.com>
2017-03-05 23:53:47 +0300

    Add shared_ptr.

--- main.cpp
+++ main.cpp
@@ -3,20 +3,32 @@
 
 #include "smart_ptr.h"
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
-    cout << "Smart pointers." << endl;
-    es::unique_ptr<std::vector<int>> sp1(new std::vector<int>{42});
-    es::unique_ptr<std::vector<int>> sp2 = sp1;
-    sp1 = sp1;
-    std::cout << "Object sp2: " << (*sp2).size() << std::endl;
-    sp1 = sp2;
-    std::cout << "Object sp1: " << (*sp1).size() << std::endl;
-    es::unique_ptr<std::vector<int>> sp3;
-    sp3 = sp1;
-    std::cout << "Object sp3: " << sp3->size() << std::endl;
+    std::cout << "Smart pointers." << std::endl;
+
+    std::cout << "Part 1. Unique pointers." << std::endl;
+    {
+        es::unique_ptr<std::vector<int>> up1(new std::vector<int>{42});
+        es::unique_ptr<std::vector<int>> up2 = up1;
+        up1 = up1;
+        std::cout << "Object up2: " << (*up2).size() << std::endl;
+        up1 = up2;
+        std::cout << "Object up1: " << (*up1).size() << std::endl;
+        es::unique_ptr<std::vector<int>> up3;
+        up3 = up1;
+        std::cout << "Object sp3: " << up3->size() << std::endl;
+    }
+
+    std::cout << "\nPart 2. Shared pointers." << std::endl;
+    {
+        es::shared_ptr<int> sp1(new int(1));
+        es::shared_ptr<int> sp2, sp3;
+        sp2 = sp1;
+        sp3 = sp1;
+        es::shared_ptr<int> sp4(new int(1));
+        es::shared_ptr<int> sp5(new int(1));
+    }
 
     return 0;
 }
--- shared_ptr.h
+++ shared_ptr.h
@@ -1,6 +1,82 @@
 #ifndef SHARED_PTR_H
 #define SHARED_PTR_H
 
+namespace es{
 
+template<typename T>
+class shared_ptr
+{
+public:
+    explicit shared_ptr(T* obj = nullptr)
+        : m_obj(obj)
+        , m_refs(new size_t(0))
+    {
+        if(m_obj) ++*m_refs;
+        std::cout << "ctor(T*), refs = " << *m_refs << std::endl;
+    }
+    shared_ptr(shared_ptr& other)
+        : m_obj(other.m_obj)
+        , m_refs(other.m_refs)
+    {
+        std::cout << "ctor(sp<T>&), refs = " << *m_refs << " -> ";
+        ++*m_refs;
+        std::cout << *m_refs << std::endl;
+    }
+    ~shared_ptr()
+    {
+        std::cout << "dtor() obj = " << m_obj << ", refs = " << *m_refs << " -> ";
+        if(*m_refs)
+        {
+            --*m_refs;
+            if(!*m_refs) delete m_obj;
+        }
+        std::cout << *m_refs;
+        if(!*m_refs)
+        {
+            std::cout << ". Delete object.";
+            delete m_refs;
+        }
+        std::cout << std::endl;
+    }
+    shared_ptr& operator=(shared_ptr& other)
+    {
+        std::cout << "operator=(), refs = " << *m_refs << " -> ";
+        if(other != *this)
+        {
+            m_obj = other.m_obj;
+            m_refs = other.m_refs;
+            ++*m_refs;
+        }
+        std::cout << *m_refs << std::endl;
+    }
+    bool operator==(const shared_ptr& other) const
+    {
+        return other.m_obj == m_obj;
+    }
+    bool operator!=(const shared_ptr& other) const
+    {
+        return other.m_obj != m_obj;
+    }
+    T& operator*() const
+    {
+        if(m_obj)
+        {
+            return *m_obj;
+        } else throw std::runtime_error("Null pointer.");
+    }
+    T* operator->() const
+    {
+        if(m_obj)
+        {
+            return m_obj;
+        } else throw std::runtime_error("Null pointer.");
+    }
+
+private:
+    T* m_obj;
+    mutable size_t* m_refs;
+};
+
+} // namespace es
 
 #endif // SHARED_PTR_H
